85853083905b20c78c1d20921b3f7436a343da88
swautelet <swautele@student.s19.be>
2022-07-14 12:44:28 +0200

    save

---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,23 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <string>
+# include <iostream>
+
+class ClapTrap{
+	public:
+		ClapTrap(std::string name);
+		~ClapTrap();
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	berepaired(unsigned int amount);
+	
+	private:
+		std::string _name;
+		int	_hp;
+		int _ep;
+		int	_attack;
+
+};
+
+#endif
